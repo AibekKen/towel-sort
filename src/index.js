183af68a5909a0c5c6ed10d4f9719288dfad7c0f
Aibek <ai.bek@mail.ru>
2022-02-23 11:58:18 +0600

    task done

--- src/index.js
+++ src/index.js
@@ -5,17 +5,12 @@ module.exports = function towelSort(matrix = []) {
    let result = [];
 
    for (let i = 0; i < matrix.length; i++) {
-      if (i % 2 === 0) {
-         for (let j = 0; j => matrix[i].length; j++) {
-            result.push(matrix[i][j]);
-         }
-      }
-      else {
-         for (let j = matrix[i].length - 1; j => 0; j--) {
-            result.push(matrix[i][j]);
-         }
+
+      for (let j = 0; j < matrix[i].length; j++) {
+         let columnInx = i % 2 === 0 ? j : matrix[i].length - j - 1;
+         result.push(matrix[i][columnInx]);
       }
    }
-
    return result;
+
 }
